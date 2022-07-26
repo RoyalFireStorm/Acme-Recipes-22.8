e6db4f12b1fbb22d42885404a73a432cf5ab9d57
RoyalFireStorm3 <alvsangon2@alum.us.es>
2022-07-26 16:33:13 +0200

    Task 001: done.

---
+++ src/test/java/acme/testing/employer/duty/EmployerDutyUpdateTest.java
@@ -0,0 +1,68 @@
+/*
+ * EmployerDutyUpdateTest.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.testing.employer.duty;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class EmployerDutyUpdateTest extends TestHarness {
+
+	// Lifecycle management ---------------------------------------------------
+
+	// Test cases -------------------------------------------------------------
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/employer/job/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void updatePositive(final int recordIndex, final String reference, final String title, final String deadline, final String salary, final String score, final String moreInfo, final String description) {
+		super.signIn("employer1", "employer1");
+
+		super.clickOnMenu("Employer", "List my jobs");
+
+		super.checkColumnHasValue(recordIndex, 0, reference);
+
+		super.clickOnListingRecord(recordIndex);
+
+		super.fillInputBoxIn("reference", reference);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("deadline", deadline);
+		super.fillInputBoxIn("salary", salary);
+		super.fillInputBoxIn("score", score);
+		super.fillInputBoxIn("moreInfo", moreInfo);
+		super.fillInputBoxIn("description", description);
+
+		super.clickOnSubmit("Update");
+
+		super.checkColumnHasValue(recordIndex, 0, reference);
+		super.checkColumnHasValue(recordIndex, 1, deadline);
+		super.checkColumnHasValue(recordIndex, 2, title);
+
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkInputBoxHasValue("reference", reference);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("deadline", deadline);
+		super.checkInputBoxHasValue("salary", salary);
+		super.checkInputBoxHasValue("score", score);
+		super.checkInputBoxHasValue("moreInfo", moreInfo);
+		super.checkInputBoxHasValue("description", description);
+
+		super.signOut();
+	}
+
+	// Ancillary methods ------------------------------------------------------
+
+}
