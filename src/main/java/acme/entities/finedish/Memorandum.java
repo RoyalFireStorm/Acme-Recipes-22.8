f63164edc0aefbcdd8f2e56a7361ad92ff3f6393
RoyalFireStorm <alvsangon2@alum.us.es>
2022-08-19 17:47:41 +0200

    Task #15: done
    Creado Memorandum y actualizado model.uxf

---
+++ src/main/java/acme/entities/finedish/Memorandum.java
@@ -0,0 +1,66 @@
+/*
+ * Duty.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.entities.finedish;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Memorandum extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp="^([A-Z]{2}:)?[A-Z]{3}-[0-9]{3}$:[0-9]{4}")
+	protected String			code;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				time;
+	
+	@NotBlank
+	protected String			report;
+	
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected FineDish				finedish;
+
+}
